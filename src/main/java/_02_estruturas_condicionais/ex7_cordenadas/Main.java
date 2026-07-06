4f2adc46525b2ab98ade977d50845dc0316bbeab
Alfredo Lopes <AlfredoLopes7@protonmail.com>
2026-07-06 16:51:26 -0300

    ex...

---
+++ src/main/java/_02_estruturas_condicionais/ex7_cordenadas/Main.java
@@ -0,0 +1,29 @@
+package src.main.java._02_estruturas_condicionais.ex7_cordenadas;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double x;
+        double y;
+
+        System.out.print("Digite o valor de x: ");
+        x = sc.nextDouble();
+        System.out.print("Digite o valor de y: ");
+        y = sc.nextDouble();
+
+        if (x == 0.0 || y == 0.0) {
+            System.out.println("Origem");
+        } else if (x == 0.0) {
+            System.out.println("está no eixo y");
+        } else if (y == 0.0) {
+            System.out.println("está no eixo x");
+        }
+
+    }
+}
