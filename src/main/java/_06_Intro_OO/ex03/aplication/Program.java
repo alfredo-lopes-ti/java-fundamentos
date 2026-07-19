a1694561c5e801fdd57e5550ff4cddb21c4821c8
Alfredo Lopes <AlfredoLopes7@protonmail.com>
2026-07-19 15:42:41 -0300

    Atualizando arquivos

---
+++ src/main/java/_06_Intro_OO/ex03/aplication/Program.java
@@ -0,0 +1,14 @@
+package _06_Intro_OO.ex03.aplication;
+
+import _06_Intro_OO.ex03.entities.Product;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
+}
