3fd32691286a6e6a8b14a8021b20a17a1783040b
Alfredo Lopes <AlfredoLopes7@protonmail.com>
2026-07-20 11:37:09 -0300

    Atualizando arquivos

--- src/main/java/_06_Intro_OO/ex03/aplication/Program.java
+++ src/main/java/_06_Intro_OO/ex03/aplication/Program.java
@@ -1,13 +1,45 @@
 package _06_Intro_OO.ex03.aplication;
 
-import _06_Intro_OO.ex03.entities.Product;
 import java.util.Locale;
 import java.util.Scanner;
+import _06_Intro_OO.ex03.entities.Product;
 
 public class Program {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner scanner = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Enter product data:");
+
+        System.out.print("Name: ");
+        product.name = scanner.nextLine();
+
+        System.out.print("Price: ");
+        product.price = scanner.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        product.quantity = scanner.nextInt();
+
+        System.out.println();
+        System.out.println(product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantityToAdd = scanner.nextInt();
+        product.addProducts(quantityToAdd);
+
+        System.out.println();
+        System.out.println("Updated product data: " + product);
+
+        System.out.println();
+        System.out.println("Enter the number of products to be removed from stock: ");
+        int quantityToRemove = scanner.nextInt();
+        product.removeProducts(quantityToRemove);
+
+        System.out.println();
+        System.out.println("Updated product data: " + product);
 
         scanner.close();
     }
