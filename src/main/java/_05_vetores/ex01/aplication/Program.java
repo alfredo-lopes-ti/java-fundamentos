071038ec020ae48f4fce128f80544e5971cc598c
Alfredo Lopes <Alfredo.Lopes.TI@gmail.com>
2026-07-26 13:56:52 -0300

    Atualizando arquivos

--- src/main/java/_05_vetores/ex01/aplication/Program.java
+++ src/main/java/_05_vetores/ex01/aplication/Program.java
@@ -10,7 +10,25 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        
+        System.out.println("How many number you are going to enter?: ");
+        int n = sc.nextInt();
+
+        int [] vect = new int [n];
+
+        for (int i = 0; i < n; i ++) {
+            System.out.println("Type a number: ");
+            vect [i] = sc.nextInt();
+        }
+
+        System.out.println("Negative numbers: ");
+
+        for (int i = 0; i < n; i ++) {
+            if (vect[i] < 0) {
+                System.out.printf("%d\n", vect[i]);
+            }
+        }
+
+        sc.close();
     }
     
 }
---
+++ src/main/java/_05_vetores/ex02/aplication/Program.java
@@ -0,0 +1,45 @@
+package _05_vetores.ex02.aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        double sum, media;
+
+        System.out.println("How many numbers are you going to enter? ");
+        n = sc.nextInt();
+
+        double[] vect = new double[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter a number: ");
+            vect[i] = sc.nextDouble();
+        }
+
+        sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum = sum + vect[i];
+        }
+
+        media = sum / n;
+
+        System.out.print("Valores: ");
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%.1f", vect[i]);
+        }
+
+        System.out.printf("\nSOMA = %.2f\n", sum);
+        System.out.printf("MEDIA = %.2f\n", media);
+
+    }
+
+}
